c5c35ee4fd10f45d0c2eb37058bc91fbfaca9f18
정필성 <jeongps@jeongpilseongs-MacBook-Pro.local>
2026-02-27 16:22:01 +0900

    part03/chapter-04: Ch 04. 이벤트 처리

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,31 @@
 <script setup>
 import { ref } from 'vue'
 
-const name = ref('')
+const newTodo = ref('')
+const todos = ref([])
+
+function addTodo() {
+  if (newTodo.value.trim()) {
+    todos.value.push(newTodo.value)
+    newTodo.value = ''
+  }
+}
+
+function removeTodo(index) {
+  todos.value.splice(index, 1)
+}
 </script>
 
 <template>
-  <h2>인사말 생성기</h2>
-  <input v-model="name" placeholder="이름을 입력하세요" />
-  <p v-if="name">안녕하세요, {{ name }}님!</p>
+  <h2>할 일 목록</h2>
+  <form @submit.prevent="addTodo">
+    <input v-model="newTodo" placeholder="할 일 입력" />
+    <button type="submit">추가</button>
+  </form>
+  <ul>
+    <li v-for="(todo, i) in todos" :key="i">
+      {{ todo }}
+      <button @click="removeTodo(i)">삭제</button>
+    </li>
+  </ul>
 </template>
