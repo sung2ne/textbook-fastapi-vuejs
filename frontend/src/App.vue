9a6c331417d5ef0c1fff142d35f4dc3f7ff496f5
정필성 <jeongps@jeongpilseongs-MacBook-Pro.local>
2026-02-27 16:22:01 +0900

    part04/chapter-05: Ch 05. 라이프사이클 훅

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,31 +1,13 @@
 <script setup>
 import { ref } from 'vue'
+import Timer from './components/Timer.vue'
 
-const newTodo = ref('')
-const todos = ref([])
-
-function addTodo() {
-  if (newTodo.value.trim()) {
-    todos.value.push(newTodo.value)
-    newTodo.value = ''
-  }
-}
-
-function removeTodo(index) {
-  todos.value.splice(index, 1)
-}
+const showTimer = ref(true)
 </script>
 
 <template>
-  <h2>할 일 목록</h2>
-  <form @submit.prevent="addTodo">
-    <input v-model="newTodo" placeholder="할 일 입력" />
-    <button type="submit">추가</button>
-  </form>
-  <ul>
-    <li v-for="(todo, i) in todos" :key="i">
-      {{ todo }}
-      <button @click="removeTodo(i)">삭제</button>
-    </li>
-  </ul>
+  <button @click="showTimer = !showTimer">
+    타이머 {{ showTimer ? '숨기기' : '보이기' }}
+  </button>
+  <Timer v-if="showTimer" />
 </template>
---
+++ frontend/src/components/Timer.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref, onMounted, onUnmounted } from 'vue'
+
+const seconds = ref(0)
+let timer = null
+
+onMounted(() => {
+  timer = setInterval(() => {
+    seconds.value++
+  }, 1000)
+})
+
+onUnmounted(() => {
+  clearInterval(timer)
+  console.log('타이머가 정리되었습니다')
+})
+</script>
+
+<template>
+  <p>경과 시간: {{ seconds }}초</p>
+</template>
