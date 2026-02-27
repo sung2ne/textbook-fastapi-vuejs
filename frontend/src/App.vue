b2295cd6207191997b9ad871b3986c66d6bead6b
정필성 <jeongps@jeongpilseongs-MacBook-Pro.local>
2026-02-27 16:22:01 +0900

    part01/chapter-04: Ch 04. Vue.js Hello World

---
+++ frontend/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+
+const message = ref('Hello, Vue.js!')
+</script>
+
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  color: #42b883;
+  text-align: center;
+  margin-top: 100px;
+}
+</style>
