3091e6814d48ba280348bf9a7900b4c48ad0e3e4
정필성 <jeongps@jeongpilseongs-MacBook-Pro.local>
2026-02-27 16:22:02 +0900

    part09/chapter-03: Ch 03. Element Plus 설치와 설정

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,10 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import ko from 'element-plus/dist/locale/ko.mjs'
 
 const pinia = createPinia()
 
 createApp(App)
   .use(pinia)
   .use(router)
+  .use(ElementPlus, { locale: ko })
   .mount('#app')
